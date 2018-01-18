9fd2f84bb8ac951707859d82f2cda6870ab6f338
zzzhr <zzzhr@hotmail.com>
2018-01-18 16:01:06 +0800

    use jwt

--- src/app.js
+++ src/app.js
@@ -6,18 +6,22 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 let ApiException = require('./app/exception/api_exception')
-let session = require('express-session');
-let RedisStore = require('connect-redis')(session);
+//let session = require('express-session');
+//let RedisStore = require('connect-redis')(session);
+let jwt = require('express-jwt')
 
 var app = express()
 
 // session..
+/*
 app.use(session({
     store: new RedisStore({ 'host': '172.18.130.158', 'port': '6396' }),
     secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true
 }));
+*/
+app.use(jwt({ secret: 'shhhhhhared-secret'}))
 // common config
 app.use(logger('dev'))
 app.use(cookieParser())
