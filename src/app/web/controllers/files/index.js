3310ad9c9ec63090683f4b973c4c36b6bc55c9e8
zzzhr <zzzhr@hotmail.com>
2018-01-25 06:48:56 +0800

    add list

--- src/app/web/controllers/files/index.js
+++ src/app/web/controllers/files/index.js
@@ -16,29 +16,62 @@ router.post('/list', (req, res) => {
 })
 
 router.post('/page', (req, res) => {
-    ResponseUtil.Ok(req, res, req.user)
+    //ResponseUtil.Ok(req, res, req.user)
+    var pageStr = req.body['page'] + ''
+    if (!validator.isInt(pageStr)) {
+        pageStr = '1'
+    }
+    var page = parseInt(pageStr)
+    if (page < 1) {
+        page = 1
+    }
+    //
+    var pageSizeStr = req.body['pageSize'] + ''
+    if (!validator.isInt(pageSizeStr)) {
+        pageStr = '20'
+    }
+    var pageSize = parseInt(pageSizeStr)
+    if (pageSize < 1) {
+        page = 20
+    }
+    if (pageSize > 999) {
+        page = 999
+    }
+    var parent = req.body['parent']
+    if (!parent) {
+        parent = ''
+    }
+    let userId = req.user.uuid
+    userFileService.rpc.listDirectoryPage(parent, userId, 0, page, pageSize).then((result) => ResponseUtil.Ok(req, res, result))
+        .catch((error) => {
+            if (error['innerCode']) {
+                ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
+            } else {
+                ResponseUtil.Error(req, res, error)
+            }
+        })
 })
 
 router.post('/createDirectory', (req, res) => {
     // ResponseUtil.Ok(req, res, req.user)
     var parent = req.body['parent']
-    if(!parent){
+    if (!parent) {
         parent = ''
     }
     let userId = req.user.uuid
     var name = req.body['name']
-    if(!name){
+    if (!name) {
         name = "New Directory Created by:" + new Date()
     }
     // call rpc
-    userFileService.rpc.createDirectory(parent,userId,name).then((result) => ResponseUtil.Ok(req, res, result))
-    .catch((error) => {
-        if (error['innerCode']) {
-            ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
-        } else {
-            ResponseUtil.Error(req, res, error)
-        }
-    })
+    userFileService.rpc.createDirectory(parent, userId, name).then((result) => ResponseUtil.Ok(req, res, result))
+        .catch((error) => {
+            if (error['innerCode']) {
+                ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
+            } else {
+                ResponseUtil.Error(req, res, error)
+            }
+        })
 })
 
 module.exports = router
