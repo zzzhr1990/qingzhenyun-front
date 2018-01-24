18ba5a63a11f7fd037f4f404be8d9ac486695ba7
zzzhr <zzzhr@hotmail.com>
2018-01-25 03:40:43 +0800

    only files

--- src/app/web/controllers/index.js
+++ src/app/web/controllers/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
 //router.use('/test', require('./test'))
-router.use('/user', require('./user'))
+//router.use('/user', require('./user'))
 router.use('/files', require('./files'))
 module.exports = router
