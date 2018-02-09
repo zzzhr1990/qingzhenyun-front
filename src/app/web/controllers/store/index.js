c0ad6e2774cb3152aec2b608495c60c75e2d012c
zzzhr <zzzhr@hotmail.com>
2018-02-09 14:57:53 +0800

    add flash pass

--- src/app/web/controllers/store/index.js
+++ src/app/web/controllers/store/index.js
@@ -15,22 +15,42 @@ router.post('/token', (req, res) => {
     let userId = req.user.uuid
     var name = req.body.name
     var parent = req.body.parent
+    var hash = req.body.hash
+    if (!hash) {
+        hash = req.body.fileHash
+    }
     if (!parent) {
         parent = ''
     }
     if (!name) {
         name = "qzy-upload-noname." + (new Date()).getTime().toString() + ".tmp"
     }
-    // check file name and directory.
+    // check file exists.
     userFileRpc.couldCreateFile(parent, userId, name, CONSTANTS.FILE_TYPE).then(cdata => {
-        cloudStoreRpc.createUploadToken(userId, parent, name).then((result) => ResponseUtil.Ok(req, res, result))
-            .catch((error) => {
-                if (error['innerCode']) {
-                    ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
-                } else {
-                    ResponseUtil.Error(req, res, error)
-                }
+        //could create
+        if (hash) {
+            cloudStoreRpc.getFile(hash).then(fileData => {
+                // exists,moving file...
+                // result['exists'] = true
+                // move file...
+                createUserFile(req,
+                    res,
+                    parent,
+                    userId,
+                    name,
+                    hash,
+                    fileData['fileSize'],
+                    fileData['mime'],
+                    fileData['preview'],
+                    0
+                )
+            }).catch(exception => {
+                createNewFileToken(req, res, parent, userId, name)
             })
+        } else {
+            createNewFileToken(req, res, parent, userId, name)
+        }
+
     }).catch(error => {
         if (error['innerCode']) {
             ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
@@ -39,6 +59,18 @@ router.post('/token', (req, res) => {
         }
     })
 })
+
+const createNewFileToken = (req, res, parent, userId, name) => {
+    cloudStoreRpc.createUploadToken(userId, parent, name).then((result) => {
+        ResponseUtil.Ok(req, res, result)
+    }).catch((error) => {
+        if (error['innerCode']) {
+            ResponseUtil.ApiError(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
+        } else {
+            ResponseUtil.Error(req, res, error)
+        }
+    })
+}
 
 router.post('/callback/wcsm3u8', (req, res) => {
     if (!req.body) {
@@ -170,40 +202,17 @@ router.get('/play/:encoded', (req, res) => {
     }
 })
 
-router.get('/play', (req, res) => {
-    let dat = '{"id":"2033a59f29d8750a41f6a7c405df9d7cde58","code":3,"inputkey":"cloud_store/FuUiV649pTiy9QyAr5Ybyv113TU1.mp4","channelname":null,"inputbucket":"other-storage","inputfsize":783469,"desc":"fileOperateSucceed","separate":0,"items":[{"cmd":"avthumb/m3u8/hlsKey/Asvibu2nTuqcgQ-vXWOTiCswSwudJz7A2hNqvv8ngYZbnvYLOdwBz_atMl6z5juaAn5XVjdKUBKu-YjatXEEKLiViAHTk5tb4ablphkxPyJju9eyRT8NqVw643WPL3KbFm8TgvB7_uvsWkUd2IBL75wUQWoVFzS_diOU7kvRBXeTMf_Zh1iQmyjKGaqdrcRFuykBfGsXc3PtsYt7nxLG3Mugp34dAr_HBtwOzFQWTFcMUhNNTU-WDlK-Pz_3cWB1tzCfCUsPK4DiBCnni7BJIBveQF70oQ8uRNROMGGkwROYszFqJrPsv2YU5uLlXoDguXkB58yWxi8s_TRdXw==/hlsKeyUrl/aHR0cDovL2FwaS5ibG9nMTIwLmNvbS92MS9zdG9yZS9wbGF5L2V5Sm9ZWE5vSWpvZ0lrWjFWV2xXTmpRNWNGUnBlVGxSZVVGeU5WbGllWFl4TVROVVZURWlMQ0FpZEdGemExOXBaQ0k2SURJMGZRPT0=/segtime/5/vcodec/libx264/acodec/libfaac/s/922x678/autoscale/1|saveas/b3RoZXItc3RvcmFnZTpwbGF5X3ByZXZpZXcvMjQvRnVVaVY2NDlwVGl5OVF5QXI1WWJ5djExM1RVMS9GdVVpVjY0OXBUaXk5UXlBcjVZYnl2MTEzVFUxLTcyMC5tM3U4","code":"3","costTime":0,"desc":"fileOperateSucceed","error":null,"fsize":357,"hash":"FlRP-JizVG8SCGbdE-ESpTwJ6Gav","key":"other-storage:play_preview/24/FuUiV649pTiy9QyAr5Ybyv113TU1/FuUiV649pTiy9QyAr5Ybyv113TU1-720.m3u8","url":"http://other.qiecdn.com/play_preview/24/FuUiV649pTiy9QyAr5Ybyv113TU1/FuUiV649pTiy9QyAr5Ybyv113TU1-720.m3u8","duration":8.467,"bit_rate":"357396","resolution":"922X678","detail":[{"fsize":357,"hash":"FlRP-JizVG8SCGbdE-ESpTwJ6Gav","key":"other-storage:play_preview/24/FuUiV649pTiy9QyAr5Ybyv113TU1/FuUiV649pTiy9QyAr5Ybyv113TU1-720.m3u8","url":"http://other.qiecdn.com/play_preview/24/FuUiV649pTiy9QyAr5Ybyv113TU1/FuUiV649pTiy9QyAr5Ybyv113TU1-720.m3u8","duration":8.467,"bit_rate":"357396","resolution":"922X678"}]}]}'
-    let callback = JSON.parse(dat)
-    var success = false
-    let videos = []
-    var durationCount = 0
-    for (let single of callback["items"]) {
-        if (single["code"] != "3" && single['key']) {
-            console.error("Task piece %s failed convert. info %s", taskId, JSON.stringify(single))
+const createUserFile = (req, res, parent, userId, name, storeId, size, mime, preview, fileType) => {
+    userFileRpc.createUserFile(parent, userId, name, storeId, size, mime, preview, fileType).then(fileData => {
+        ResponseUtil.Ok(req, res, fileData)
+    }).catch(fileError => {
+        if (fileError['innerCode']) {
+            ResponseUtil.ApiErrorAsOk(req, res, new ApiException(fileError['innerMessage'], 400, fileError['innerMessage']))
         } else {
-            success = true
-            let duration = parseFloat(single["duration"])
-            if (durationCount < duration) {
-                durationCount = duration
-            }
-            let rawKey = single['key']
-            let idx = rawKey.indexOf(':')
-            let bucket = rawKey.substring(0, idx)
-            let key = rawKey.substring(idx + 1, rawKey.length)
-            let storeType = 0
-            let clear = rawKey.substring(rawKey.lastIndexOf('-') + 1, rawKey.lastIndexOf('.'))
-            videos.push({
-                'duration': duration,
-                'bucket': bucket,
-                'key': key,
-                'storeType': storeType,
-                'clear': clear
-            })
+            ResponseUtil.Error(req, res, fileError)
         }
-    }
-
-    console.log(JSON.stringify(videos))
-    res.send("1234567890123456")
-})
+    })
+}
 
 router.post('/callback/wcs', (req, res) => {
     // console.log('WCS Callback %s', req.body.callbackBody)
@@ -221,16 +230,7 @@ router.post('/callback/wcs', (req, res) => {
         let mime = data.mime
         let preview = data.preview
         let fileType = 0
-        userFileRpc.createUserFile(parent, userId, name, storeId, size, mime, preview, fileType).then(fileData => {
-            ResponseUtil.Ok(req, res, fileData)
-        }).catch(fileError => {
-            if (fileError['innerCode']) {
-                ResponseUtil.ApiErrorAsOk(req, res, new ApiException(fileError['innerMessage'], 400, fileError['innerMessage']))
-            } else {
-                ResponseUtil.Error(req, res, fileError)
-            }
-        })
-
+        createUserFile(req, res, parent, userId, name, storeId, size, mime, preview, fileType)
     }).catch(error => {
         if (error['innerCode']) {
             ResponseUtil.ApiErrorAsOk(req, res, new ApiException(error['innerMessage'], 400, error['innerMessage']))
