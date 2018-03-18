561a96243f4a6f92f8de8d9d3565c81335eb6c35
zzzhr <zzzhr@hotmail.com>
2018-03-18 21:47:00 +0800

    fix cannot add task

--- src/app/web/controllers/offline/index.js
+++ src/app/web/controllers/offline/index.js
@@ -294,6 +294,7 @@ router.post('/start', (req, res) => {
                 JSON.stringify(addon),
                 '',
                 '',
+                '',
                 current,
                 current,
                 '', 0,
